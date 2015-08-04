89575a2d484d463cd977ca483e2a23913de4db10
Zephor 武智辉 <zephor@qq.com>
2015-08-04 14:08:39 +0800

    Update Constants.java
    init

--- src/main/java/cms/ds/nginx/Constants.java
+++ src/main/java/cms/ds/nginx/Constants.java
@@ -9,14 +9,14 @@ package cms.ds.nginx;
  */
 public final class Constants {
     
-    public static final String ZOOKEEPER = "172.16.7.105:2181";
+    public static final String ZOOKEEPER = "172.16.0.100:2181";
     
-    public static final String KAFKA_TOPIC = "f-ds";
+    public static final String KAFKA_TOPIC = "test";
     
     public static final String CONSUMER_GROUP = "storm";
     
-    public static final String MONGO_HOST = "172.16.7.105";
+    public static final String MONGO_HOST = "172.16.0.100";
     
-    public static final String MONGO_DB = "f-ds";
+    public static final String MONGO_DB = "test";
 
 }
